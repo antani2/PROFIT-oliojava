1505cf15f37072dcf3a3bae8e4a9916b2d3b2a4f
Marko Back <marko.back@tieto.com>
2015-02-04 14:07:55 +0200

    Java4

---
+++ src/java4/Sales.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package java4;
+
+/**
+ * @author profit-guest01
+ *
+ */
+public class Sales extends Employee {
+
+    /**
+     * @param n
+     * @param s
+     */
+    public Sales(String n, int s) {
+        super(n, s);
+        // TODO Auto-generated constructor stub
+    }
+
+}
